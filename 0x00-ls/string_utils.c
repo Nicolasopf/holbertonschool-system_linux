b406a94be21e1f7b6bd9c0c3f2d855331fa80c30
Nicolas <n.ico15935746@gmail.com>
2021-07-14 01:51:45 -0500

    [Added] File where string functions are in

--- 0x00-ls/string_utils.c
+++ 0x00-ls/string_utils.c
@@ -1,13 +1,38 @@
 #include "ls.h"
 
+/**
+ * strcmp: Compare two strings to check if are equal.
+ * @x: first string.
+ * @y: second string.
+ * Return: 0 if are equal, otherwise 1.
+*/
+
 int strcmp(const char *x, const char *y)
 {
 	while (*x)
 	{
 		if (*x != *y)
-            return (1);
+			return (1);
 		x++;
 		y++;
 	}
 	return (0);
+}
+
+/*
+ * strchar: Check if the char is in the str.
+ * @str: string.
+ * @a: char to check.
+ * Return: 0 if is there, 1 if not.
+ */
+
+int strchar(char *str, char a)
+{
+	while (*str)
+	{
+		if (*str == a)
+			return (0);
+		str++;
+	}
+	return (1);
 }
