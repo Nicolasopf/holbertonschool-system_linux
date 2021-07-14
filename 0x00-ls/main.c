3f85d1cd8cc1215f34b8c0823dab6b8517c026db
Nicolas <n.ico15935746@gmail.com>
2021-07-14 18:10:49 -0500

    [Updated] Moved function to options.

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	char options[1024];
-	/* char *file_arguments[1024]; */
+/*	char *files_names[1024] = {'\0'}; Arguments that are not options. */
 
 	options[0] = '\0';
 	check_arguments(argc, argv, options);
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
  * @argc: argument counter.
  * @argv: argument vector.
  * @options: pointer where to save the options to use.
+ * @files_names: pointer where to save the name of files passed as arguments.
  * Return: 0.
  */
 
@@ -50,50 +51,6 @@ int check_arguments(int argc, char *argv[], char *options)
 
 	options[char_counter] = '\0';
 	return (0);
-}
-
-/**
- * simple_ls - Print files as usually, no options.
- * @argc: Argument counter.
- * @argv: Argument vectors.
- * Return: 0 if not errors, otherwise 2.
- */
-
-int simple_ls(int argc, char *argv[])
-{
-	DIR *dir;
-	struct dirent *read;
-	int counter = 1, r_value = 0;
-
-	if (argc == 1)
-		argv[1] = ".", argc++;
-
-	for (; counter < argc; counter++)
-	{
-		dir = opendir(argv[counter]);
-
-		if (!dir)
-		{
-			r_value = display_error(argv[0], argv[counter]);
-			continue;
-		}
-		if (argc > 2)
-			printf("%s:\n", argv[counter]);
-
-		while ((read = readdir(dir)) != NULL)
-		{
-			if (*read->d_name == '.')
-				continue;
-			printf("%s  ", read->d_name);
-		}
-
-		if (argc > 2 && counter != argc - 1)
-			printf("\n");
-
-		printf("\n");
-		closedir(dir);
-	}
-	return (r_value);
 }
 
 /**
