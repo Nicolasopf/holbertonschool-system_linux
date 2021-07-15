1ea0aa11b72ea018f95217d9da8b6108937ff086
Nicolas <n.ico15935746@gmail.com>
2021-07-14 19:59:51 -0500

    [Added] New prototype for the options handling

--- 0x00-ls/ls.h
+++ 0x00-ls/ls.h
@@ -11,7 +11,8 @@ int display_error(char *name, char *dir_name);
 int ls_a(int argc, char *argv[]);
 int strchar(char *str, char a);
 int check_arguments(int argc, char *argv[], char *options);
-int simple_ls(int argc, char *argv[]);
+int simple_ls(int argc, char *argv[], char *options);
 int get_files(int argc, char *argv[], char *files[], char *not_files[]);
+void print_in(DIR *dir, char *options);
 
 #endif /* LS_H */
