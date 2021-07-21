031c4511c0995dd539fbcb7f48356fcf97dce4e3
Nicolas <n.ico15935746@gmail.com>
2021-07-21 14:18:33 -0500

    [Added] Header file

---
+++ 0x01-getline/_getline.h
@@ -0,0 +1,11 @@
+#ifndef _GET_LINE_H
+#define _GET_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char *_getline(const int fd);
+
+#endif /* _GET_LINE_H */
