9fa6aa732a6583f3dcdb2e53624646b2b3f913f0
Nicolas <n.ico15935746@gmail.com>
2021-07-23 16:25:53 -0500

    [Added] prototypes for linked list

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -6,6 +6,26 @@
 #include <unistd.h>
 #include <string.h>
 
+#define READ_SIZE 409600
+
+/**
+*
+*
+*
+*/
+
+typedef struct file_n
+{
+	int fd;
+	char *next_line;
+	char buff[READ_SIZE];
+	struct file_n *next;
+} file_t;
+
 char *_getline(const int fd);
+void free_all(file_t *head);
+file_t *new_node(file_t **head, const int fd);
+file_t *search_node(file_t *head, const int fd);
+
 
 #endif /* _GET_LINE_H */
